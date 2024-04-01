3a579f84da69c4ba6779eee0d9e06581290006a1
Xhelliom <stephane.wetterwald@gmail.com>
2024-04-01 14:57:06 +0200

    change title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Wetterland documentation',
+  tagline: 'Documentation sur le déploiement de divers outils IT',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
